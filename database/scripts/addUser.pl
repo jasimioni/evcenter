487fdd5b8bd2942c20c30e900b33a6f6b3678852
Joao Andre Simioni <jasimioni@gmail.com>
2015-06-21 18:16:07 -0700

    Add User Script

---
+++ database/scripts/addUser.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use common::sense;
+use DBI;
+use Digest::SHA1 qw/sha1_base64/;
+
+my ($username, $password) = @ARGV;
+
+if (! defined $username || ! defined $password) {
+    print "Usage: $0 <username> <newpassword>\n";
+    exit 1;
+}
+
+my $dbh = DBI->connect("dbi:Pg:dbname=evcenter", '', '', { AutoCommit => 1 });
+my $sth = $dbh->prepare('INSERT INTO uc_users (username, password) VALUES (?, ?)');
+$sth->execute($username, sha1_base64($password));
